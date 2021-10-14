75c73defe2ad1677c46ae5fe8c8422881cd7099e
rabberto <rafael.rbberto@gmail.com>
2021-10-14 15:40:57 -0300

    Estrutura CEP UF MUNICIPIO

---
+++ src/Api.Data.Test/UfGets.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Data.Context;
+using Api.Data.Implementations;
+using Api.Data.Text;
+using Api.Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Api.Data.Test
+{
+    public class UfGets : BaseTest, IClassFixture<DbTest>
+    {
+        private ServiceProvider _serviceProvide;
+
+        public UfGets(DbTest dbTest)
+        {
+            _serviceProvide = dbTest.ServiceProvider;
+        }
+
+        [Fact(DisplayName = "Gets de UF")]
+        [Trait("GETs", "UfEntity")]
+        public async Task E_Possivel_Realizar_Gets_UF()
+        {
+            using (var context = _serviceProvide.GetService<MyContext>())
+            {
+                UfImplementation _repositorio = new UfImplementation(context);
+                UfEntity _entity = new UfEntity
+                {
+                    Id = new Guid("e7e416de-477c-4fa3-a541-b5af5f35ccf6"),
+                    Sigla = "SP",
+                    Nome = "São Paulo"
+                };
+
+                var _registroExiste = await _repositorio.ExistAsync(_entity.Id);
+                Assert.True(_registroExiste);
+
+                var _registroSelecionado = await _repositorio.SelectAsync(_entity.Id);
+                Assert.NotNull(_registroSelecionado);
+                Assert.Equal(_entity.Sigla, _registroSelecionado.Sigla);
+                Assert.Equal(_entity.Nome, _registroSelecionado.Nome);
+                Assert.Equal(_entity.Id, _registroSelecionado.Id);
+
+                var _todosRegistros = await _repositorio.SelectAllAsync();
+                Assert.NotNull(_todosRegistros);
+                Assert.True(_todosRegistros.Count() == 27);
+
+            }
+
+        }
+
+
+    }
+}
---
+++ src/Api.Data/Implementations/UfImplementation.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Data.Context;
+using Api.Data.Repository;
+using Api.Domain.Entities;
+using Api.Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Implementations
+{
+    public class UfImplementation : BaseRepository<UfEntity>, IUfRepository
+    {
+        private DbSet<UfEntity> _dataset;
+
+        public UfImplementation(MyContext context) : base(context)
+        {
+            _dataset = context.Set<UfEntity>();
+        }
+
+        public Task SelectAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
